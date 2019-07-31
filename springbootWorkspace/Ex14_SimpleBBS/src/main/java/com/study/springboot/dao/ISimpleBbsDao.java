9641cb0fe56bad395c4d92f61c485339376b1d21
fixel.sy <fixel.sy@fixelsoft.com>
2019-07-31 17:50:20 +0900

    - JdbcTemplate : 간단한 게시판(BBS) 만들기

---
+++ springbootWorkspace/Ex14_SimpleBBS/src/main/java/com/study/springboot/dao/ISimpleBbsDao.java
@@ -0,0 +1,12 @@
+package com.study.springboot.dao;
+
+import java.util.List;
+
+import com.study.springboot.dto.SimpleBbsDto;
+
+public interface ISimpleBbsDao {
+	public List<SimpleBbsDto> listDao();	//리스트를 보는 것
+	public SimpleBbsDto viewDao(String id);	//한개의 글 내용을 보는 것
+	public int writeDao(String writer, String title, String content);	//글 쓰기
+	public int deleteDao(String id);		//글 삭제
+}
